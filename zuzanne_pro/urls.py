fbb77022476945084d95d264e317a00677c421c4
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-06-30 23:40:08 +0530

    Serve media files in production

--- zuzanne_pro/urls.py
+++ zuzanne_pro/urls.py
@@ -1,12 +1,25 @@
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.urls import include, path
+
+# urlpatterns = [
+#     path('', include('zuzanne_app.urls')),
+# ]
+
+# handler404 = 'zuzanne_app.views.custom_404_view'
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('zuzanne_app.urls')),
+    path("", include("zuzanne_app.urls")),
 ]
 
-handler404 = 'zuzanne_app.views.custom_404_view'
+handler404 = "zuzanne_app.views.custom_404_view"
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Always serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
