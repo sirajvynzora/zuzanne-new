577a740e883570079636807af5707a8d43ff62f4
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-07-01 10:06:11 +0530

    Ready for Render deployment

--- zuzanne_pro/settings.py
+++ zuzanne_pro/settings.py
@@ -84,7 +84,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
