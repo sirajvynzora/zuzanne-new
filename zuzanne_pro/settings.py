9969fee502eff384491fda8d61dd04f72294ff5c
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-07-01 10:48:14 +0530

    ready to deploye

--- zuzanne_pro/settings.py
+++ zuzanne_pro/settings.py
@@ -79,6 +79,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
